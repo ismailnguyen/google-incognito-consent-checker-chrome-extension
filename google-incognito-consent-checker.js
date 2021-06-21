a56084678f97bb3ff108195e62e555a3458a1533
Ismaïl <nguyen.ismail@gmail.com>
2021-06-21 10:53:21 +0200

    add facebook popins

--- google-incognito-consent-checker.js
+++ google-incognito-consent-checker.js
@@ -1,8 +1,10 @@
 var checkConsents = function () {
 	if (!document || !document.location || !document.location.origin) return;
-		
+	
+	var locationOrigin = document.location.origin;
+
 	// Youtube
-	if (document.location.origin.indexOf('youtube.com') > -1) {
+	if (locationOrigin.indexOf('youtube.com') > -1) {
 		// Homepage
 		if (document.querySelector('*[aria-label^="Agree"]'))
 			document.querySelector('*[aria-label^="Agree"]').click();
@@ -13,10 +15,21 @@ var checkConsents = function () {
 	}
 		
 	// Google
-	if (document.location.origin.indexOf('google.com') > -1) {
+	if (locationOrigin.indexOf('google.com') > -1) {
 		// Search page
 		if (document.getElementById('zV9nZe'))
 			document.getElementById('zV9nZe').click();
+	}
+	
+	// Facebook
+	if (locationOrigin.indexOf('facebook.com') > -1) {
+		// Login popin
+		if (document.getElementById('expanding_cta_close_button'))
+			document.getElementById('expanding_cta_close_button').click();
+		
+		// Cookie consent
+		if (document.querySelector('*[data-cookiebanner^="accept_button"]'))
+			document.querySelector('*[data-cookiebanner^="accept_button"]').click();
 	}	
 }
 
